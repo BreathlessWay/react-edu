0a9918c18823d33f69f6f591a2863812ddd57304
BreathlessWay <731005087@qq.com>
2019-09-15 08:14:10 +0800

    add picture

--- todo-list/my-app-advance/src/pages/AppPage/index.jsx
+++ todo-list/my-app-advance/src/pages/AppPage/index.jsx
@@ -2,6 +2,7 @@ import React, { Component, lazy, Suspense } from 'react';
 // 从react-router-dom中引入BrowserRouter和Route
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import SuspenseLoading from '../../components/SuspenseLoading';
+import ErrorBoundaryComponent from '../../components/ErrorBoundaryComponent';
 
 import { TodoListContext, defaultTodoList } from '../../context';
 
@@ -57,22 +58,24 @@ export default class AppPage extends Component {
   };
 
   render () {
-    return <TodoListContext.Provider value={this.state.todo}>
-      <BrowserRouter>
-        <Suspense fallback={<SuspenseLoading/>}>
-          <article>
-            <nav className='nav-wrap'>
-              <Link to={'/'} replace={true}>待办首页</Link>
-            </nav>
-            <Switch>
-              {/*HomePage和TodoDetailPage通过Route的render挂载到路由中，并且必须将路由相关的props手动传入组件*/}
-              <Route path="/" exact component={HomePage}/>
-              <Route path="/detail/:id" component={TodoDetailPage}/>
-              <Route component={NotFoundPage}/>
-            </Switch>
-          </article>
-        </Suspense>
-      </BrowserRouter>
-    </TodoListContext.Provider>;
+    return <ErrorBoundaryComponent>
+      <TodoListContext.Provider value={this.state.tod}>
+        <BrowserRouter>
+          <Suspense fallback={<SuspenseLoading/>}>
+            <article>
+              <nav className='nav-wrap'>
+                <Link to={'/'} replace={true}>待办首页</Link>
+              </nav>
+              <Switch>
+                {/*HomePage和TodoDetailPage通过Route的render挂载到路由中，并且必须将路由相关的props手动传入组件*/}
+                <Route path="/" exact component={HomePage}/>
+                <Route path="/detail/:id" component={TodoDetailPage}/>
+                <Route component={NotFoundPage}/>
+              </Switch>
+            </article>
+          </Suspense>
+        </BrowserRouter>
+      </TodoListContext.Provider>
+    </ErrorBoundaryComponent>;
   }
 }
