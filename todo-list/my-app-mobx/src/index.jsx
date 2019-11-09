750b087cb72bebc5bba868ddca6762c701eb5cd5
BreathlessWay <731005087@qq.com>
2019-11-09 21:23:38 +0800

    add

--- todo-list/my-app-mobx/src/index.jsx
+++ todo-list/my-app-mobx/src/index.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {configure} from 'mobx'
 
+import AppPage from './pages/AppPage';
+
 import './index.scss';
 
 configure({
   enforceActions: 'observed'
-})
-
-import AppPage from './pages/AppPage';
+});
 
 ReactDOM.render(<AppPage name="todo-list"/>, document.getElementById('root'));
 
